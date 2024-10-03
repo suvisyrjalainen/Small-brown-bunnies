ba702c0ff3331845575a12afdd15ec736a66509b
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2024-10-03 18:42:34 +0300

    Bunny is moving

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,33 @@
+
+function preload() {
+  backgroundImage = loadImage("landscape.jpg");
+  bunny = loadImage("bunny.png");
+}
+
+
+
 function setup() {
-    createCanvas(windowWidth, 400);
+    createCanvas(windowWidth, windowWidth / 3);
+    bunny1 = new Bunny();
   }
-  function draw() {
+  
+function draw() {
     background("brown");
+    image(backgroundImage, 0, 0, windowWidth, windowWidth / 3);
+    bunny1.move();
+}
+
+class Bunny {
+  constructor() {
+    this.x = 0;
+    this.y = 100;
+    this.width = 50;
+    this.height = 50;
+    this.Xspeed = 2;
   }
+  
+  move() {
+    this.x = this.x + this.Xspeed;
+    image(bunny, this.x, this.y, this.width, this.height);
+  }
+}
---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log(`Server running http://localhost:${port}`);
+}
+);
