e9e6de1dce18c1828f4ded3adcc422166e2d2da1
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2024-10-16 09:45:07 +0300

    paddle created

--- public/scripts.js
+++ public/scripts.js
@@ -10,12 +10,13 @@ function preload() {
 function setup() {
     createCanvas(windowWidth, windowWidth / 3);
     createBunnies();
+    
   }
   
 function draw() {
     background("brown");
     image(backgroundImage, 0, 0, windowWidth, windowWidth / 3);
-    
+    createPaddle();
 
     bunnyList.forEach(function(bunnyObject, index) {
       bunnyObject.move();
@@ -23,8 +24,12 @@ function draw() {
       if (bunnyObject.x > windowWidth) {
         bunnyList.splice(index, 1);
       }
-      
+
     });
+}
+
+function createPaddle() {
+  rect(30, windowWidth / 3 - 50, windowWidth/20, windowWidth/40);
 }
 
 
